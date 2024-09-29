0d43e10e217fb5cbd1d4b8f8c33a0054d3085bed
Alon Yaskil <alonyaskil@Alons-MacBook-Pro.local>
2024-09-28 21:33:32 -0700

    handle different paths

--- src/lib/tableTypes.ts
+++ src/lib/tableTypes.ts
@@ -1,33 +1,44 @@
-import { Asset } from "../api/api"
+import { Asset, NestedOwner } from "../api/api";
 
+type ColumnId = "_id" | "assetName" | "owner.name" | "enriched.isCrownJewel";
 export interface Column {
-    id: "_id" | "assetName" | "owner.name" | "enriched.isCrownJewel"
-    label: string
-    minWidth?: number
+  id: ColumnId;
+  label: string;
+  minWidth?: number;
 }
 
 export const columns: readonly Column[] = [
-    {
-        id: "_id",
-        label: "ID",
-        minWidth: 170,
-    },
-    {
-        id: "assetName",
-        label: "Asset Name",
-        minWidth: 100,
-    },
-    {
-        id: "owner.name",
-        label: "Owner Name",
-        minWidth: 170,
-    },
-    {
-        id: "enriched.isCrownJewel",
-        label: "Is Crown Jewel",
-        minWidth: 170,
-    },
-]
+  {
+    id: "_id",
+    label: "ID",
+    minWidth: 170,
+  },
+  {
+    id: "assetName",
+    label: "Asset Name",
+    minWidth: 100,
+  },
+  {
+    id: "owner.name",
+    label: "Owner Name",
+    minWidth: 170,
+  },
+  {
+    id: "enriched.isCrownJewel",
+    label: "Is Crown Jewel",
+    minWidth: 170,
+  },
+];
 
-export const getColumnValue = (obj: Asset, path: string) =>
-    path.split(".").reduce((acc, key) => (acc as any)?.[key], obj) as any
+export const getColumnValue = (asset: Asset, path: string) => {
+  if (
+    path === "owner.name" &&
+    (asset.owner as NestedOwner).owner !== undefined
+  ) {
+    return (asset.owner as NestedOwner).owner.name;
+  }
+
+  return path
+    .split(".")
+    .reduce((acc, key) => (acc as any)?.[key], asset) as any;
+};
